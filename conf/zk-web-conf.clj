c7fb9a3dcacc9b0a3b262befb6e8d236f53cf70e
Jagpreet <jagpreet@hike.in>
2017-03-15 17:56:27 +0530

    added option to restrict access to only few email ids while using google oauth

--- conf/zk-web-conf.clj
+++ conf/zk-web-conf.clj
@@ -3,9 +3,11 @@
  :users {"admin" "hello"}
  :default-node ""
  :g-client-id  "Your Google OAuth2 clientID"
- :g-redirect-url "Your redirect address for OAuh2"
+ :g-redirect-url "Your redirect address for OAuth2"
  :g-client-secret "Your Google OAuth2 ClientSecret"
  :g-hd "your org domain as registered on gmail" ;provide only if you have google mail for your domain and want to restrict login to only ids on this domain.
  :zk-ip2group {:ip1 :grp1 :ip2 :grp2 :ip3 :grp1} ;map ips to a group.(both ip and groupname are keywords that is start with a ":" followed by value. Like ":value" without quotes) (here ip1 and ip3 are in grp1
  :zk-slack-hooks {:grp1 "Slack hook to post changes to zk for grp1" :grp2 "Slack hook to post changes to zk for grp2"} ;when changes are made to zk with connection string having ip in group "grp", updates are logged to slack using hook corresponding to ":grp"
+ :allowed-email-ids #{} ; set of email ids allowed to have access. Works only when :restrict-to-ids is true
+ :restrict-to-ids false ; set this to true to restrict access to few ids, listed in set :allowed-email-ids
 }
--- src/zk_web/google_login.clj
+++ src/zk_web/google_login.clj
@@ -5,22 +5,25 @@
 			  [cheshire.core :as json-parser]
 			  [noir.response :as resp]
 			  [zk-web.pages :refer [on-login-success]]
-			  [zk-web.conf :as conf] ) 
+			  [zk-web.conf :as conf] )
 	(:use [noir.core])
 	)
 
 (def CLIENT_ID (:g-client-id (conf/load-conf)) )
 (def REDIRECT_URI (:g-redirect-url (conf/load-conf)) )
 (def CLIENT_SECRET (:g-client-secret (conf/load-conf)) )
- 
+(def DOMAIN (:g-hd (conf/load-conf)))
+(def ALLOWED_EMAIL_IDS (:allowed-email-ids (conf/load-conf)) )
+(def RESTRICT_TO_IDS (:restrict-to-ids (conf/load-conf)) )
+
 (def redirect-url (str "https://accounts.google.com/o/oauth2/auth?"
               "scope=email%20profile&"
               "redirect_uri=" (ring.util.codec/url-encode REDIRECT_URI) "&"
               "response_type=code&"
               "client_id=" (ring.util.codec/url-encode CLIENT_ID) "&"
               "approval_prompt=force&"
-              (if (nil? (:g-hd (conf/load-conf)) ) "" (str "hd=" (:g-hd (conf/load-conf))) ) ))
- 
+              (if (nil? DOMAIN ) "" (str "hd=" DOMAIN) ) ))
+
 (defn google [code]
  (if-not (nil? code)
 	 (let [access-token-response (http-client/post "https://www.googleapis.com/oauth2/v3/token"
@@ -31,18 +34,17 @@
 	                                           :grant_type "authorization_code"}})
 	       user-details (json-parser/parse-string (:body (http-client/get (str "https://www.googleapis.com/oauth2/v1/userinfo?access_token="
 	 (get (json-parser/parse-string (:body access-token-response)) "access_token")))))]
-	 (if (.endsWith (get user-details "email") "@hike.in") 
+	 (if (and (or (nil? DOMAIN ) (.endsWith (get user-details "email") (str "@" DOMAIN))) (or (not RESTRICT_TO_IDS) (contains? ALLOWED_EMAIL_IDS (get user-details "email"))))
 	 	(do ( on-login-success (get user-details "name") (get user-details "email") )
 	 		(resp/redirect "/"))
 	 	(render [:get "/login"]
- 			{:msg "Please Use only Hike Email Ids." :target "/"}) )) 
+ 			{:msg "Please Use only Hike Email Ids." :target "/"}) ))
 
  	 (render [:get "/login"]
  		{:msg "Something went wrong. Try again." :target "/"}) ))
- 
+
 (defpage "/google" {:keys [code]}
   (google code) )
- 
+
 (defpage "/gauth" []
     (resp/redirect redirect-url) )
-
